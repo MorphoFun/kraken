919a8c8417f85354b0d0231a3add6e0ab3d70e78
Sandy Kawano <sandy.kawano@gmail.com>
2018-12-18 14:28:05 -0800

    remove vegan
    git status

--- R/BiomechanicsFunctions.R
+++ R/BiomechanicsFunctions.R
@@ -60,7 +60,7 @@ pcsa <- function(mass, pennationAngle, fascicleLength, density = 1060, ...) {
 #' profilePlotR(subset(AT_Kine2, Variable == "AbductAdductAngle"), "PercentStance", "value", groupname = "Appendage", subgroupname = "Ind", rowname = "Filename", highlight = AT_Kine2_AAA_subset, title = "Abduction versus Adduction", xlab = "PercentStance", ylab = "Degrees")
 #'
 #' @import ggplot2
-#' @import vegan
+#' 
 #' @export
 
 profilePlotR <- function(d = d, xname = xname, yname = yname, groupname = groupname, subgroupname = subgroupname, rowname = rowname, title = "plot", xlab = "x", ylab = "y", colorlinesby = subgroupname, highlight = NULL, ...) {
