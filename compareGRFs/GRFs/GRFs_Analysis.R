62cf10f747ca15054e04dc0fb3fdb440cb3c9329
Sandy Kawano <sandy.kawano@gmail.com>
2019-11-14 12:52:24 -0500

    Fix Issues with GRFanalysis function

--- compareGRFs/GRFs/GRFs_Analysis.R
+++ compareGRFs/GRFs/GRFs_Analysis.R
@@ -30,7 +30,7 @@ VideoFile <- data.frame(read.csv("./dataraw/FinLimbGRFs_VideoInfo.csv", header=T
 
 
 #### LOADING THE DATA ####
-setwd("./dataraw")
+setwd("./dataraw/force_Raw")
 
 fileList <- list.files(pattern = ".txt", full.names = FALSE)
 myFiles <- lapply(fileList, FUN = read.table)
@@ -57,23 +57,24 @@ GRFanalysis <- function(myData) {
   #### LOOKING UP THE VIDEO INFO ####
   # Appendages listed as "Both" have both pectoral and pelvic appendage dat
   VideoInfo <- VideoFile[VideoFile$File.name %in% Trial,]
-  VideoInfo$Pectoral.Start.Frame <- as.numeric(VideoInfo$Pectoral.Start.Frame)
-  VideoInfo$Pectoral.End.Frame <- as.numeric(VideoInfo$Pectoral.End.Frame)
-  VideoInfo$Pelvic.Start.Frame <- as.numeric(VideoInfo$Pelvic.Start.Frame)
-  VideoInfo$Pelvic.End.Frame <- as.numeric(VideoInfo$Pelvic.End.Frame)
+  # VideoInfo$Pectoral.Start.Frame <- as.numeric(VideoInfo$Pectoral.Start.Frame)
+  # VideoInfo$Pectoral.End.Frame <- as.numeric(VideoInfo$Pectoral.End.Frame)
+  # VideoInfo$Pelvic.Start.Frame <- as.numeric(VideoInfo$Pelvic.Start.Frame)
+  # VideoInfo$Pelvic.End.Frame <- as.numeric(VideoInfo$Pelvic.End.Frame)
   Date <- format(as.Date(VideoInfo$Date.Filmed, format = "%m/%d/%y"), format="%y%m%d")
   
   #### LOOKING UP THE CALIB INFO ####
   CalibInfo <- CalibFile[CalibFile$Date %in% Date,]
-  
   
   #### PECTORAL APPENDAGE ####
   
   ## Selecting pectoral trials
   Pec_VideoInfo <- VideoInfo[!VideoInfo$Appendages == "Pelvic",]
   Pec_Data <- myData[names(myData) %in% Pec_VideoInfo$File.name]
+
   
-  
+  Pec_VideoInfo_Trial <- NULL
+  Pec_CalibInfo_Trial <- NULL
   Pec_GRFs <- NULL
   saveAs <- NULL
   Pec_GRFs_Filtered <- NULL
@@ -85,66 +86,69 @@ GRFanalysis <- function(myData) {
   GraphTitle <- NULL
   
   for (i in 1:length(Pec_Data)) {
+    Pec_VideoInfo_Trial[[i]] <- data.frame(Pec_VideoInfo[Pec_VideoInfo$File.name == names(Pec_Data)[i],])
+    Pec_CalibInfo_Trial[[i]] <- CalibInfo[as.character(CalibInfo$Filename) == as.character(Pec_VideoInfo_Trial[[i]]$Calib_Force),]
+    
     ## Converting LabView output to GRFs ##
-    Pec_GRFs[[i]] <- voltToForce(Pec_Data[[i]], CalibInfo[i,3:5], zeroStart = Pec_VideoInfo[i,]$Pectoral.Start.Frame, lightStartFrame = Pec_VideoInfo[i,]$Light.Start, 
-                                 startFrame = Pec_VideoInfo[i,]$Pectoral.Start.Frame, endFrame = Pec_VideoInfo[i,]$Pectoral.End.Frame, filename = Pec_VideoInfo[i,]$File.name, BW = Pec_VideoInfo[i,]$Body.Weight.kg)
-    names(Pec_GRFs)[[i]] <- Pec_VideoInfo[i,]$File.name
+    Pec_GRFs[[i]] <- voltToForce(Pec_Data[[i]], calib = Pec_CalibInfo_Trial[[i]][3:5], zeroStart = Pec_VideoInfo_Trial[[i]]$Pectoral.Start.Frame, lightStartFrame = Pec_VideoInfo_Trial[[i]]$Light.Start, 
+                                 startFrame = Pec_VideoInfo_Trial[[i]]$Pectoral.Start.Frame, endFrame = Pec_VideoInfo_Trial[[i]]$Pectoral.End.Frame, filename = Pec_VideoInfo_Trial[[i]]$File.name, BW = Pec_VideoInfo_Trial[[i]]$Body.Weight.kg)
+    names(Pec_GRFs)[[i]] <- names(Pec_Data)[[i]]
     
     ## Filter the data
-    saveAs[i] <- paste(Pec_VideoInfo[i,]$File.name, "_Pec_Filtered.pdf", sep = "")
+    saveAs[i] <- paste(Pec_VideoInfo_Trial[[i]]$File.name, "_Pec_Filtered.pdf", sep = "")
     Pec_GRFs_Filtered[[i]] <- butterFilteR(Pec_GRFs[[i]], saveAs = saveAs[[i]], saveGraph = "yes")  
-    names(Pec_GRFs_Filtered)[[i]] <- Pec_VideoInfo[i,]$File.name
+    names(Pec_GRFs_Filtered)[[i]] <- names(Pec_Data)[[i]]
     
     ## Calculating angles of GRF orientation
     Pec_GRFs_Filtered_dataset[[i]] <- GRFAngles(Pec_GRFs_Filtered[[i]]$GRF0Sum_filter_interp)
-    names(Pec_GRFs_Filtered_dataset)[[i]] <- Pec_VideoInfo[i,]$File.name
+    names(Pec_GRFs_Filtered_dataset)[[i]] <- names(Pec_Data)[[i]]
     
     ## Converting to units of body weight
-    BW_N[i] <- Pec_VideoInfo[i,]$Body.Weight.kg*9.8 # converting animal's body weight from kilograms to Newtons
+    BW_N[i] <- Pec_VideoInfo_Trial[[i]]$Body.Weight.kg*9.8 # converting animal's body weight from kilograms to Newtons
     Pec_GRFs_Filtered_dataset[[i]]$InterpV_BW <- Pec_GRFs_Filtered_dataset[[i]]$InterpV_N/BW_N[i]
     Pec_GRFs_Filtered_dataset[[i]]$InterpML_BW <- Pec_GRFs_Filtered_dataset[[i]]$InterpML_N/BW_N[i]
     Pec_GRFs_Filtered_dataset[[i]]$InterpAP_BW <- Pec_GRFs_Filtered_dataset[[i]]$InterpAP_N/BW_N[i]
     Pec_GRFs_Filtered_dataset[[i]]$NetGRF_BW <- Pec_GRFs_Filtered_dataset[[i]]$NetGRF_N/BW_N[i]
     
     ## Remove data when pectoral and pelvic appendages overlap
-    Pec_GRFs_Filtered_dataset_noOverlap[[i]] <- removeOverlaps(Pec_GRFs_Filtered_dataset[[i]], Pec_VideoInfo[i,2:3], Pec_VideoInfo[i,4:5], Pec_VideoInfo[i,]$Filming.Rate.Hz)
+    Pec_GRFs_Filtered_dataset_noOverlap[[i]] <- removeOverlaps(Pec_GRFs_Filtered_dataset[[i]], Pec_VideoInfo_Trial[[i]][2:3], Pec_VideoInfo_Trial[[i]][4:5], Pec_VideoInfo_Trial[[i]]$Filming.Rate.Hz)
     
     ## Evaluating at peak/max net GRF
     ## Also making sure that the peak does not occur during portions where the limbs overlap on the force plate
     Pec_GRFs_Filtered_dataset_noOverlap_Peak[[i]] <- Pec_GRFs_Filtered_dataset_noOverlap[[i]][which.max(Pec_GRFs_Filtered_dataset_noOverlap[[i]]$NetGRF_BW),]
     
     ## Saving the graphs
-    pdfSave[i] <- paste(Pec_VideoInfo[i,]$File.name, "_Pec_PostProcess.pdf", sep = "")
+    pdfSave[i] <- paste(Pec_VideoInfo_Trial[[i]]$File.name, "_Pec_PostProcess.pdf", sep = "")
     pdf(pdfSave[[i]])
-    
+
     par(mfrow=c(2,2), oma = c(3, 0, 2, 0))  # oma = outer margin with 2 lines above the top of the graphs
-    
+
     # Vertical component of GRF graph
     plot(Pec_GRFs_Filtered_dataset[[i]]$PercentStance, Pec_GRFs_Filtered_dataset[[i]]$InterpV_BW, xlab='Percent Stance', ylab='GRF - Vertical (BW)', main='Zeroed GRF (Vertical) Force', type="l", col="black")
     lines(Pec_GRFs_Filtered_dataset_noOverlap[[i]]$PercentStance, Pec_GRFs_Filtered_dataset_noOverlap[[i]]$InterpV_BW, type="l", col="blue", lwd = 4)
     abline(v=Pec_GRFs_Filtered_dataset_noOverlap_Peak[[i]]$PercentStance, col='grey30', lty=2, lwd=2) # Plotting vertical line at Peak Net GRF
-    
+
     # Mediolateral component of GRF graph
     plot(Pec_GRFs_Filtered_dataset[[i]]$PercentStance, Pec_GRFs_Filtered_dataset[[i]]$InterpML_BW, xlab='Percent Stance', ylab='GRF - Mediolateral (BW)', main='Zeroed GRF (Mediolateral) Force', type="l", col="black")
     lines(Pec_GRFs_Filtered_dataset_noOverlap[[i]]$PercentStance, Pec_GRFs_Filtered_dataset_noOverlap[[i]]$InterpML_BW, type="l", col="red", lwd = 4)
     abline(v=Pec_GRFs_Filtered_dataset_noOverlap_Peak[[i]]$PercentStance, col='grey30', lty=2, lwd=2)
-    
+
     # Horizontal (Anteroposterior) component of GRF graph
     plot(Pec_GRFs_Filtered_dataset[[i]]$PercentStance, Pec_GRFs_Filtered_dataset[[i]]$InterpAP_BW, xlab='Percent Stance', ylab='GRF - Anteroposterior (BW)', main='Zeroed GRF (Anteroposterior) Force', type="l", col="black")
     lines(Pec_GRFs_Filtered_dataset_noOverlap[[i]]$PercentStance, Pec_GRFs_Filtered_dataset_noOverlap[[i]]$InterpAP_BW, type="l", col="forestgreen", lwd = 4)
     abline(v=Pec_GRFs_Filtered_dataset_noOverlap_Peak[[i]]$PercentStance, col='grey30', lty=2, lwd=2)
-    
+
     # Net GRF graph
     plot(Pec_GRFs_Filtered_dataset[[i]]$PercentStance, Pec_GRFs_Filtered_dataset[[i]]$NetGRF_BW, xlab='Percent Stance', ylab='Net GRF (BW)', main='Zeroed Net GRF Force', type="l", col="black")
     lines(Pec_GRFs_Filtered_dataset_noOverlap[[i]]$PercentStance, Pec_GRFs_Filtered_dataset_noOverlap[[i]]$NetGRF_BW, type="l", col="purple", lwd = 4)
     abline(v=Pec_GRFs_Filtered_dataset_noOverlap_Peak[[i]]$PercentStance, col='grey30', lty=2, lwd=2)
-    
+
     GraphTitle[i] <- pdfSave[i]
     mtext(GraphTitle[i], line=0.5, outer=TRUE)  # writes an overall title over the graphs
     mtext('Dashed grey line = % Stance for Peak Net GRF', side=1, outer=TRUE, col = 'grey30')
     mtext('Black lines indicate times with more than 1 structure on plate', side=1, line=1.5, outer=TRUE, col = 'black')
     dev.off()
-    
+
   }
   
   ## Collapsing data at the peak net GRF into a single data.frame
@@ -157,6 +161,8 @@ GRFanalysis <- function(myData) {
   Pel_VideoInfo <- VideoInfo[!VideoInfo$Appendages == "Pectoral",]
   Pel_Data <- myData[names(myData) %in% Pel_VideoInfo$File.name]
   
+  Pel_VideoInfo_Trial <- NULL
+  Pel_CalibInfo_Trial <- NULL
   Pel_GRFs <- NULL
   Pel_saveAs <- NULL
   Pel_GRFs_Filtered <- NULL
@@ -166,67 +172,69 @@ GRFanalysis <- function(myData) {
   Pel_GRFs_Filtered_dataset_noOverlap_Peak <- NULL
   
   for (i in 1:length(Pel_Data)) {
+    Pel_VideoInfo_Trial[[i]] <- data.frame(Pel_VideoInfo[Pel_VideoInfo$File.name == names(Pel_Data)[i],])
+    Pel_CalibInfo_Trial[[i]] <- CalibInfo[as.character(CalibInfo$Filename) == as.character(Pel_VideoInfo_Trial[[i]]$Calib_Force),]
     
     ## Converting LabView output to GRFs ##
-    Pel_GRFs[[i]] <- voltToForce(Pel_Data[[i]], CalibInfo[i,3:5], zeroStart = Pel_VideoInfo[i,]$Pelvic.Start.Frame, lightStartFrame = Pel_VideoInfo[i,]$Light.Start, 
-                                 startFrame = Pel_VideoInfo[i,]$Pelvic.Start.Frame, endFrame = Pel_VideoInfo[i,]$Pelvic.End.Frame, filename = Pel_VideoInfo[i,]$File.name, BW = Pel_VideoInfo[i,]$Body.Weight.kg)
-    names(Pel_GRFs)[[i]] <- Pel_VideoInfo[i,]$File.name
+    Pel_GRFs[[i]] <- voltToForce(Pel_Data[[i]], Pel_CalibInfo_Trial[[i]][3:5], zeroStart = Pel_VideoInfo_Trial[[i]]$Pelvic.Start.Frame, lightStartFrame = Pel_VideoInfo_Trial[[i]]$Light.Start, 
+                                 startFrame = Pel_VideoInfo_Trial[[i]]$Pelvic.Start.Frame, endFrame = Pel_VideoInfo_Trial[[i]]$Pelvic.End.Frame, filename = Pel_VideoInfo_Trial[[i]]$File.name, BW = Pel_VideoInfo_Trial[[i]]$Body.Weight.kg)
+    names(Pel_GRFs)[[i]] <- names(Pel_Data)[[i]]
     
     ## Filter the data
-    Pel_saveAs[i] <- paste(Pel_VideoInfo[i,]$File.name, "_Pel_Filtered.pdf", sep = "")
+    Pel_saveAs[i] <- paste(Pel_VideoInfo_Trial[[i]]$File.name, "_Pel_Filtered.pdf", sep = "")
     Pel_GRFs_Filtered[[i]] <- butterFilteR(Pel_GRFs[[i]], saveAs = Pel_saveAs[[i]], saveGraph = "yes")  
-    names(Pel_GRFs_Filtered)[[i]] <- Pel_VideoInfo[i,]$File.name
+    names(Pel_GRFs_Filtered)[[i]] <- names(Pel_Data)[[i]]
     
     ## Calculating angles of GRF orientation
     Pel_GRFs_Filtered_dataset[[i]] <- GRFAngles(Pel_GRFs_Filtered[[i]]$GRF0Sum_filter_interp)
-    names(Pel_GRFs_Filtered_dataset)[[i]] <- Pel_VideoInfo[i,]$File.name
+    names(Pel_GRFs_Filtered_dataset)[[i]] <- names(Pel_Data)[[i]]
     
     ## Converting to units of body weight
-    Pel_BW_N[i] <- Pel_VideoInfo[i,]$Body.Weight.kg*9.8 # converting animal's body weight from kilograms to Newtons
+    Pel_BW_N[i] <- Pel_VideoInfo_Trial[[i]]$Body.Weight.kg*9.8 # converting animal's body weight from kilograms to Newtons
     Pel_GRFs_Filtered_dataset[[i]]$InterpV_BW <- Pel_GRFs_Filtered_dataset[[i]]$InterpV_N/BW_N[i]
     Pel_GRFs_Filtered_dataset[[i]]$InterpML_BW <- Pel_GRFs_Filtered_dataset[[i]]$InterpML_N/BW_N[i]
     Pel_GRFs_Filtered_dataset[[i]]$InterpAP_BW <- Pel_GRFs_Filtered_dataset[[i]]$InterpAP_N/BW_N[i]
     Pel_GRFs_Filtered_dataset[[i]]$NetGRF_BW <- Pel_GRFs_Filtered_dataset[[i]]$NetGRF_N/BW_N[i]
     
     ## Remove data when pectoral and pelvic appendages overlap
-    Pel_GRFs_Filtered_dataset_noOverlap[[i]] <- removeOverlaps(Pel_GRFs_Filtered_dataset[[i]], Pel_VideoInfo[i,4:5], Pel_VideoInfo[i,2:3], Pel_VideoInfo[i,]$Filming.Rate.Hz)
+    Pel_GRFs_Filtered_dataset_noOverlap[[i]] <- removeOverlaps(Pel_GRFs_Filtered_dataset[[i]], Pel_VideoInfo_Trial[[i]][4:5], Pel_VideoInfo_Trial[[i]][2:3], Pel_VideoInfo_Trial[[i]]$Filming.Rate.Hz)
     
     ## Evaluating at peak/max net GRF
     ## Also making sure that the peak does not occur during portions where the limbs overlap on the force plate
     Pel_GRFs_Filtered_dataset_noOverlap_Peak[[i]] <- Pel_GRFs_Filtered_dataset_noOverlap[[i]][which.max(Pel_GRFs_Filtered_dataset_noOverlap[[i]]$NetGRF_BW),]
     
     ## Saving the graphs
-    pdfSave[i] <- paste(Pel_VideoInfo[i,]$File.name, "_Pel_PostProcess.pdf", sep = "")
+    pdfSave[i] <- paste(Pel_VideoInfo_Trial[[i]]$File.name, "_Pel_PostProcess.pdf", sep = "")
     pdf(pdfSave[[i]])
-    
+
     par(mfrow=c(2,2), oma = c(3, 0, 2, 0))  # oma = outer margin with 2 lines above the top of the graphs
-    
+
     # Vertical component of GRF graph
     plot(Pel_GRFs_Filtered_dataset[[i]]$PercentStance, Pel_GRFs_Filtered_dataset[[i]]$InterpV_BW, xlab='Percent Stance', ylab='GRF - Vertical (BW)', main='Zeroed GRF (Vertical) Force', type="l", col="black")
     lines(Pel_GRFs_Filtered_dataset_noOverlap[[i]]$PercentStance, Pel_GRFs_Filtered_dataset_noOverlap[[i]]$InterpV_BW, type="l", col="blue", lwd = 4)
     abline(v=Pel_GRFs_Filtered_dataset_noOverlap_Peak[[i]]$PercentStance, col='grey30', lty=2, lwd=2) # Plotting vertical line at Peak Net GRF
-    
+
     # Mediolateral component of GRF graph
     plot(Pel_GRFs_Filtered_dataset[[i]]$PercentStance, Pel_GRFs_Filtered_dataset[[i]]$InterpML_BW, xlab='Percent Stance', ylab='GRF - Mediolateral (BW)', main='Zeroed GRF (Mediolateral) Force', type="l", col="black")
     lines(Pel_GRFs_Filtered_dataset_noOverlap[[i]]$PercentStance, Pel_GRFs_Filtered_dataset_noOverlap[[i]]$InterpML_BW, type="l", col="red", lwd = 4)
     abline(v=Pel_GRFs_Filtered_dataset_noOverlap_Peak[[i]]$PercentStance, col='grey30', lty=2, lwd=2)
-    
+
     # Horizontal (Anteroposterior) component of GRF graph
     plot(Pel_GRFs_Filtered_dataset[[i]]$PercentStance, Pel_GRFs_Filtered_dataset[[i]]$InterpAP_BW, xlab='Percent Stance', ylab='GRF - Anteroposterior (BW)', main='Zeroed GRF (Anteroposterior) Force', type="l", col="black")
     lines(Pel_GRFs_Filtered_dataset_noOverlap[[i]]$PercentStance, Pel_GRFs_Filtered_dataset_noOverlap[[i]]$InterpAP_BW, type="l", col="forestgreen", lwd = 4)
     abline(v=Pel_GRFs_Filtered_dataset_noOverlap_Peak[[i]]$PercentStance, col='grey30', lty=2, lwd=2)
-    
+
     # Net GRF graph
     plot(Pel_GRFs_Filtered_dataset[[i]]$PercentStance, Pel_GRFs_Filtered_dataset[[i]]$NetGRF_BW, xlab='Percent Stance', ylab='Net GRF (BW)', main='Zeroed Net GRF Force', type="l", col="black")
     lines(Pel_GRFs_Filtered_dataset_noOverlap[[i]]$PercentStance, Pel_GRFs_Filtered_dataset_noOverlap[[i]]$NetGRF_BW, type="l", col="purple", lwd = 4)
     abline(v=Pel_GRFs_Filtered_dataset_noOverlap_Peak[[i]]$PercentStance, col='grey30', lty=2, lwd=2)
-    
+
     GraphTitle[i] <- pdfSave[i]
     mtext(GraphTitle[i], line=0.5, outer=TRUE)  # writes an overall title over the graphs
     mtext('Dashed grey line = % Stance for Peak Net GRF', side=1, outer=TRUE, col = 'grey30')
     mtext('Black lines indicate times with more than 1 structure on plate', side=1, line=1.5, outer=TRUE, col = 'black')
     dev.off()
-    
+
   }
   
   ## Collapsing data at the peak net GRF into a single data.frame
